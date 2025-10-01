2ff4f912881947f2b847a4ee6184d309dbbd4465
quentin-massoulle <quentin.massoulle@etud.u-picardie.fr>
2025-10-01 17:42:13 +0200

    wip

--- UniversiteDomain/UseCases/EtudiantUseCases/Create/CreateEtudiantUseCase.cs
+++ UniversiteDomain/UseCases/EtudiantUseCases/Create/CreateEtudiantUseCase.cs
@@ -19,6 +19,25 @@ public class CreateEtudiantUseCase(IEtudiantRepository etudiantRepository)
     }
     private async Task CheckBusinessRules(Etudiant etudiant)
     {
-        // à venir
+        ArgumentNullException.ThrowIfNull(etudiant);
+        ArgumentNullException.ThrowIfNull(etudiant.NumEtud);
+        ArgumentNullException.ThrowIfNull(etudiant.Email);
+        ArgumentNullException.ThrowIfNull(etudiantRepository);
+        
+        // On recherche un étudiant avec le même numéro étudiant
+        List<Etudiant> existe = await etudiantRepository.FindByConditionAsync(e=>e.NumEtud.Equals(etudiant.NumEtud));
+
+        // Si un étudiant avec le même numéro étudiant existe déjà, on lève une exception personnalisée
+        if (existe is {Count:>0}) throw new DuplicateNumEtudException(etudiant.NumEtud+ " - ce numéro d'étudiant est déjà affecté à un étudiant");
+        
+        // Vérification du format du mail
+        if (!CheckEmail.IsValidEmail(etudiant.Email)) throw new InvalidEmailException(etudiant.Email + " - Email mal formé");
+        
+        // On vérifie si l'email est déjà utilisé
+        existe = await etudiantRepository.FindByConditionAsync(e=>e.Email.Equals(etudiant.Email));
+        // Une autre façon de tester la vacuité de la liste
+        if (existe is {Count:>0}) throw new DuplicateEmailException(etudiant.Email +" est déjà affecté à un étudiant");
+        // Le métier définit que les nom doite contenir plus de 3 lettres
+        if (etudiant.Nom.Length < 3) throw new InvalidNomEtudiantException(etudiant.Nom +" incorrect - Le nom d'un étudiant doit contenir plus de 3 caractères");
     }
 }
