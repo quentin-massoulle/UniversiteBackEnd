d0442a0bc4928105daaaaf80f022a9df2795f983
quentin-massoulle <quentin.massoulle@etud.u-picardie.fr>
2025-10-15 14:37:36 +0200

    tets

---
+++ UniversiteDomaineUnitTest/EtudianrUnitTest.cs
@@ -0,0 +1,15 @@
+namespace UniversiteDomaineUnitTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
